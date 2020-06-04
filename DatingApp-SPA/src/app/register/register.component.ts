04814be7fd7ba9977d0d81e4f5b7757dba85fa74
Pecor <glenville.pecor@abbott.com>
2020-06-04 13:06:50 -0700

    EOS6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -7,20 +8,21 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  // tslint:disable-next-line: new-parens
   @Output() cancelRegister = new EventEmitter;
 
   model: any = {};
 
-  constructor(private authServuce: AuthService) { }
+  constructor(private authServuce: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register(){
     this.authServuce.register(this.model).subscribe(() => {
-      console.log('registration successful');
+      this.alertify.success('registration successful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);//idk why when I use error it does not work here
     });
   }
 
