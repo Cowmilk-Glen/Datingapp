fb6216ddee45c285adf8f9f6916834a316510447
Pecor <glenville.pecor@abbott.com>
2020-06-18 10:40:39 -0700

    testing bugs

---
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../_models/user';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+
+  users: User[];
+  constructor(private userService: UserService, private alertify: AlertifyService, private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers(){
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+}
